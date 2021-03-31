19e26cbd276be78b420124763ac1d5a30baf8ef1
OnlineTeachingNow <startonlineteachingnow@gmail.com>
2021-03-31 18:31:11 +0200

    Made the TripSpawner functional, fixed a few more bugs, and cleaned up the code

--- Assets/Scripts/TransportPathing.cs
+++ Assets/Scripts/TransportPathing.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class TransportPathing : MonoBehaviour
 {
     TripConfig _tripConfig;
+    TripSpawner _tripSpawner;
     List<Transform> _wayPoints;
     private Vector3 _original;
     private int _wayPointsIndex = 0;
 
     void Start()
     {
+        _tripSpawner = FindObjectOfType<TripSpawner>();
         _wayPoints = _tripConfig.GetWayPoints();
-        //Debug.Log("Getwaypoints: " + _wayPoints[0] + _wayPoints[1]);
         _original = _wayPoints[_wayPointsIndex].transform.position;
         this.transform.position = _original;
     }
@@ -36,18 +37,14 @@ public class TransportPathing : MonoBehaviour
             var _movementThisFrame = _tripConfig.GetMoveSpeed() * Time.deltaTime;
 
             this.transform.position = Vector3.MoveTowards(_currentPosition, _targetPosition, _movementThisFrame);
-        //  Debug.Log("this transform.position: " + this.transform.position);
-         // Debug.Log("distance: " + Vector3.Distance(transform.position, _targetPosition));
-            if (Vector3.Distance(_currentPosition, _targetPosition) < 0.08) //changed this from the trip over value to this, because it wasn't incrementing because the target waypoint was set to 
-                //North carolina and there was no way to increment the thing because it only incremented on the last trip waypoint. With the following code, it increments and then the new target
-                //waypoint becomes Colorado.
+            if (Vector3.Distance(_currentPosition, _targetPosition) < 0.08)
             {
                 _wayPointsIndex++;
-            //    Debug.Log("Are waypoints being indexed?" + _wayPointsIndex);
             }
         }
         else
         {
+            _tripSpawner.ManageLastWayPoint(true);
             Destroy(this.gameObject);
         }
     }
--- Assets/Scripts/TripSpawner.cs
+++ Assets/Scripts/TripSpawner.cs
@@ -2,68 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Test test, 1, 2, 3
 public class TripSpawner : MonoBehaviour
 {
     [SerializeField] private List<TripConfig> _trips;
     TransportPathing _transportPathing;
+    Camera _mainCamera;
     private Transform _originTransform;
     private GameObject _transportPrefab;
     TransportPathing _newTransportation;
     bool _hasReachedLastTripWaypoint = false;
-    bool _looping = false;
 
     private void Start()
     {
-
+        _mainCamera = FindObjectOfType<Camera>();
     }
     public void StartTrip()
     {
         StartCoroutine(SpawnTrips());
     }
-
-    /*
-     *The following method doesn't seem to do much when passed over, so I decided to pass it over. It made no difference to the indexing of the last trip when I put this back into code.
-    
-    private IEnumerator StartTripSpawning()
-    {
-        do
-        {
-            yield return StartCoroutine(SpawnTrips());
-        }
-        while (_looping == true);
-    }
-    */
-    
-
     private IEnumerator SpawnTrips()
     {
-        for (int tripIndex = 0; tripIndex <= _trips.Count; tripIndex++)
+        for (int tripIndex = 0; tripIndex < _trips.Count; tripIndex++)
         {
-            Debug.Log("Trip Count: " + _trips.Count);
-            Debug.Log("Trip Index: " + tripIndex);
             _hasReachedLastTripWaypoint = false;
             yield return (StartCoroutine(InstantiateTransportation(_trips[tripIndex])));
         }
     }
-
     private IEnumerator InstantiateTransportation(TripConfig trip)
     {
         _newTransportation = Instantiate(trip.GetTransportationPrefab(), trip.GetWayPoints()[0].transform.position, Quaternion.identity);
+        _mainCamera.gameObject.SetActive(false);
         _newTransportation.SetTripConfig(trip);
-       // _newTransportation.GetComponent<TransportPathing>().SetTripConfig(trip); //This made no difference to the trip indexer when put back into code.
-       /*
-        if (_transportPathing != null)
-        {
-       */
-            yield return new WaitUntil(() => _hasReachedLastTripWaypoint == true); //Doing some crazy casting from a system.fun<bool> to a regular bool. Need to look into this more.
-            Debug.Log("_transportPathing.hasReachedLastWayPoint");
-        /*}
-        else
-        {
-            yield return new WaitForSeconds(5f);
-            Debug.LogError("Transport pathing is NULL.");
-        } */ 
+        yield return new WaitUntil(() => _hasReachedLastTripWaypoint == true); //Doing some crazy casting from a system.fun<bool> to a regular bool. Need to look into this more. 
     }
 
     public bool ManageLastWayPoint(bool trueorfalse)
